8bed6951249837c606e502d88694446eb595b5cf
Joshua Leichty <joshs.leichty@gmail.com>
2020-02-16 18:15:55 -0500

    Update testFile.py

--- testFile.py
+++ testFile.py
@@ -1,8 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 16 17:15:32 2020
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
 
-@author: luked
-"""
 
-#THIS IS OUR FILE -- THIS LINE IS JUST A TEST OKAY.
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(60000,28,28,1)
+X_train = X_train.astype('float32')/255
+
+X_test = X_test.reshape(10000,28,28,1)
+X_test = X_test.astype('float32')/255
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+model = Sequential()
+
+
+model.add(Conv2D(12, kernel_size=3, activation='relu', input_shape=(28,28,1)))
+model.add(Conv2D(6, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train,validation_data=(X_test, y_test), epochs=1, batch_size = 64)
