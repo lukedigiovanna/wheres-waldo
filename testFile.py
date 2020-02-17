19ade426b59df1cbc4eb5913b92d879d716ceda4
Joshua Leichty <joshs.leichty@gmail.com>
2020-02-16 19:23:50 -0500

    Update testFile.py

--- testFile.py
+++ testFile.py
@@ -16,25 +16,33 @@ training_data = []
 not_waldo = []
 waldo = []
 def create_train():
+	counter = 0
 	for category in CATEGORIES:
 		path = os.path.join(DATADIR, category) #Path to waldo or not waldo
 		class_num = CATEGORIES.index(category)
 		for img in os.listdir(path):
 			img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE) #Convert images to grayscale arr
+
 			'''
 			If we want to resize
 			IMG_SIZE = new image size
 			new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
 			'''
-			training_data.append([img_array, class_num])
 			if class_num == 0:
 				waldo.append([img_array, class_num])
+				training_data.append([img_array, class_num])
 			elif class_num == 1:
+				counter += 1
 				not_waldo.append([img_array, class_num])
+				if counter < 39:
+					training_data.append([img_array, class_num])
+
 create_train()
 
+random.shuffle(training_data)
 X_train = []
 y_train = []
+
 for features, label in training_data:
 	X_train.append(features)
 	y_train.append(label)
@@ -43,27 +51,16 @@ X = np.array(X_train).reshape(-1, 64, 64, 1)
 X = X.astype('float32') / 255
 Y = np.asarray(y_train).astype('float32')/255
 
-'''
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+Y = to_categorical(Y)
 
-X_train = X_train.reshape(60000,28,28,1)
-X_train = X_train.astype('float32')/255
-
-X_test = X_test.reshape(10000,28,28,1)
-X_test = X_test.astype('float32')/255
-
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
 
 model = Sequential()
 
-
-model.add(Conv2D(12, kernel_size=3, activation='relu', input_shape=(28,28,1)))
+model.add(Conv2D(12, kernel_size=3, activation='relu', input_shape=(64,64,1)))
 model.add(Conv2D(6, kernel_size=3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train,validation_data=(X_test, y_test), epochs=1, batch_size = 64)
-'''
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
